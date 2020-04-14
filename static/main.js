8c9d8a649cc95eb72e96c05e0c47a5d07cbd69d5
leemurus <sidorevich.anton@gmail.com>
2020-04-14 21:18:03 +0300

    Finished web-design of page

--- static/main.js
+++ static/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
-    alert('Loaded');
 });
+
+// AJAX ==========================================================================================
 
 function getAjaxInformation(url) {
     let response = null;
@@ -32,3 +33,41 @@ function postAjaxInformation(url, data) {
     });
     return response
 }
+
+// AJAX ==========================================================================================
+
+// OPEN AND CLOSE DIVS ===========================================================================
+
+$('.create_wallet').click(function () {
+    $('.dark_background').show();
+    $('.wallets_inputs').show();
+});
+
+$('.close_wallets_inputs_button').click(function () {
+    $('.dark_background').hide();
+    $('.wallets_inputs').hide();
+});
+
+$('.create_transaction').click(function () {
+    $('.dark_background').show();
+    $('.transactions_inputs').show();
+});
+
+$('.close_transaction_inputs_button').click(function () {
+    $('.dark_background').hide();
+    $('.transactions_inputs').hide();
+});
+
+// OPEN AND CLOSE DIVS ===========================================================================
+
+
+// OPERATIONS ===========================================================================
+
+$('.transactions_inputs_form').on('submit', function () {
+   alert('WOw');
+});
+
+$('.wallets_inputs_form').on('submit', function () {
+    alert('WOW2');
+});
+
